ce5573fbe1e09fa4c7ad0cf933d6b5091302c36c
Steven Herbst <sherbst@stanford.edu>
2018-03-29 13:11:18 -0700

    pwl working for cases when all pwl segments have at least one test point (i.e., nseg << npts)

--- msemu/pwl.py
+++ msemu/pwl.py
@@ -1,15 +1,5 @@
-from numpy import genfromtxt, convolve
-import matplotlib.pyplot as plt
-import argparse
 import numpy as np
-from scipy.signal import lsim, impulse, tf2ss, fftconvolve
-from scipy.interpolate import interp1d
-from scipy.stats import describe
 import scipy.sparse
-from math import exp, log, ceil, floor, pi, log2, sqrt
-import subprocess
-from scipy.linalg import matrix_balance, svd, norm, expm, lstsq
-from numpy.linalg import lstsq, solve, inv
 import cvxpy
 import logging, sys
 
@@ -19,6 +9,9 @@ class PWL:
         self.slopes = slopes
         self.times = times
         self.dtau = dtau
+
+    def domain(self, dt):
+        return np.arange(self.times[0], self.times[-1], dt)
 
     def eval(self, pts):
         idx = np.floor((pts-self.times[0])/self.dtau).astype(int)
@@ -30,7 +23,7 @@ class Waveform:
         self.t = t
         self.v = v
 
-    def make_pwl(self, n, start=None, stop=None):
+    def make_pwl(self, start=None, stop=None, n=50):
         # assign start and stop points of representation if not given
         if start is None:
             start = self.t[0]
@@ -50,9 +43,7 @@ class Waveform:
 
         # set up matrix for optimization problem
         nchk = idx_max-idx_min+1
-        rows = np.zeros(2*nchk)
-        cols = np.zeros(2*nchk)
-        data = np.zeros(2*nchk)
+        A = scipy.sparse.dok_matrix((nchk, n+1), dtype=np.float64)
 
         for k in range(nchk):
             # time of point to check
@@ -65,45 +56,39 @@ class Waveform:
             # linear correction term
             alpha = idx_float - idx_int
 
-            # contribution from control point idx_int
-            rows[2*k+0] = k
-            cols[2*k+0] = idx_int
-            data[2*k+0] = 1 - alpha
-
-            # contribution from control point idx_int+1
-            rows[2*k+1] = k
-            cols[2*k+1] = idx_int+1
-            data[2*k+1] = alpha
+            A[k, idx_int] = 1 - alpha
+            A[k, idx_int+1] = alpha
 
         # construct the optimization problem
-        A = scipy.sparse.coo_matrix((data, (rows, cols)), shape=(nchk, n+1))
         A = cvxpy.Constant(A)
-
         b = self.v[idx_min:(idx_max+1)]
 
         # run optimization
         x = cvxpy.Variable(n+1)
-        objective = cvxpy.Minimize(cvxpy.pnorm(A * x - b, p=float('inf')))
+        objective = cvxpy.Minimize(cvxpy.pnorm(A*x - b, p=float('inf')))
         prob = cvxpy.Problem(objective)
         result = prob.solve()
+
         logging.debug('Maximum PWL fit error: {}'.format(result))
 
-        value = np.squeeze(np.asarray(x.value))
-        offsets = value[:-1]
-        slopes = np.diff(value)/dtau
+        values = np.squeeze(np.asarray(x.value))
+        offsets = values[:-1]
+        slopes = np.diff(values)/dtau
 
         return PWL(offsets=offsets, slopes=slopes, times=times, dtau=dtau)
 
 def main():
-    #logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
     t = np.linspace(0, 3, 10e3)
     v = np.exp(t)
 
     wave = Waveform(t, v)
-    pwl = wave.make_pwl(16)
+    pwl = wave.make_pwl(0.5, 2.5, 4)
 
-    plt.plot(t, v, t[:-1], pwl.eval(t[:-1]))
+    import matplotlib.pyplot as plt
+    t_eval = pwl.domain(0.01)
+    plt.plot(t, v, t_eval, pwl.eval(t_eval))
     plt.show()
     
 if __name__=='__main__':
