513ade221e5291177c7631af555c4f1f1f860e2d
Steven Herbst <sherbst@stanford.edu>
2018-04-11 12:42:45 -0700

    minor updates

---
+++ msemu/cmd.py
@@ -0,0 +1,15 @@
+import argparse
+import pathlib
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--build_dir', type=str, help='Directory where packages should be placed.')
+    parser.add_argument('--rom_dir', type=str, help='Directory where ROM files should be placed.')
+    parser.add_argument('--data_dir', type=str, help='Directory where data should be placed.')
+    parser.add_argument('--channel_dir', type=str, help='Directory where channel measurements should be placed.')
+
+    return parser
+
+def mkdir_p(dir_name):
+    pathlib.Path(dir_name).mkdir(parents=True, exist_ok=True)
