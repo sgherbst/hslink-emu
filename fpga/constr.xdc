c2cf45053ae1fad5f11abc960600b72c303cdf93
Steven Herbst <sherbst@stanford.edu>
2019-07-06 22:15:49 -0700

    vivado tcl script runs

--- fpga/constr.xdc
+++ fpga/constr.xdc
@@ -1,9 +1,17 @@
 # 200 MHz system clock
+
 set_property IOSTANDARD LVDS [get_ports SYSCLK_P]
 set_property PACKAGE_PIN H9 [get_ports SYSCLK_P]
 set_property PACKAGE_PIN G9 [get_ports SYSCLK_N]
 set_property IOSTANDARD LVDS [get_ports SYSCLK_N]
 create_clock -period 5.000 -name SYSCLK_P -waveform {0.000 2.500} -add [get_ports SYSCLK_P]
+
+# Debug hub clock
+
+set_property C_ENABLE_CLK_DIVIDER false [get_debug_cores dbg_hub]
+set_property C_USER_SCAN_CHAIN 1 [get_debug_cores dbg_hub]
+set_property C_CLK_INPUT_FREQ_HZ 30000000 [get_debug_cores dbg_hub]
+connect_debug_port dbg_hub/clk [get_nets clkgen_i/clk_wiz_0_i/clk_out1]
 
 # output		
 # (on LED 0)		
---
+++ run/fpga_build.tcl
@@ -0,0 +1,106 @@
+# Create the Vivado project
+
+create_project -force project ../build/project -part xc7z045ffg900-2
+
+# Add source files
+
+add_files [glob ../build/*.sv]
+add_files [glob ../fpga/*.sv]
+add_files [glob ../shared/*.sv]
+
+# Add clock constraints
+
+add_files -fileset constrs_1 "../fpga/constr.xdc"
+
+# Set the top-level module
+
+set_property -name top -value dut -objects [current_fileset]
+
+# Instantiate the clock wizard
+
+create_ip -name clk_wiz -vendor xilinx.com -library ip -module_name clk_wiz_0
+
+set_property -dict [list \
+    CONFIG.PRIM_SOURCE Differential_clock_capable_pin \
+    CONFIG.PRIM_IN_FREQ 200.0 \
+    CONFIG.NUM_OUT_CLKS 4 \
+    CONFIG.CLKOUT1_USED true \
+    CONFIG.CLKOUT1_REQUESTED_OUT_FREQ 30.0 \
+    CONFIG.CLKOUT2_USED true \
+    CONFIG.CLKOUT2_REQUESTED_OUT_FREQ 30.0 \
+    CONFIG.CLKOUT2_DRIVES BUFGCE \
+    CONFIG.CLKOUT3_USED true \
+    CONFIG.CLKOUT3_REQUESTED_OUT_FREQ 30.0 \
+    CONFIG.CLKOUT3_DRIVES BUFGCE \
+    CONFIG.CLKOUT4_USED true \
+    CONFIG.CLKOUT4_REQUESTED_OUT_FREQ 30.0 \
+    CONFIG.CLKOUT4_DRIVES BUFGCE \
+] [get_ips clk_wiz_0]
+
+# Create the VIO
+
+create_ip -name vio -vendor xilinx.com -library ip -module_name vio_0
+
+set_property -dict [list \
+    CONFIG.C_NUM_PROBE_IN {0} \
+    CONFIG.C_NUM_PROBE_OUT {9} \
+    CONFIG.C_PROBE_OUT0_WIDTH {1} \
+    CONFIG.C_PROBE_OUT1_WIDTH {4} \
+    CONFIG.C_PROBE_OUT2_WIDTH {4} \
+    CONFIG.C_PROBE_OUT3_WIDTH {14} \
+    CONFIG.C_PROBE_OUT4_WIDTH {14} \
+    CONFIG.C_PROBE_OUT5_WIDTH {14} \
+    CONFIG.C_PROBE_OUT6_WIDTH {41} \
+    CONFIG.C_PROBE_OUT7_WIDTH {10} \
+    CONFIG.C_PROBE_OUT8_WIDTH {10} \
+] [get_ips vio_0]
+
+# Create ila_0
+
+create_ip -name ila -vendor xilinx.com -library ip -module_name ila_0
+
+set_property -dict [list \
+    CONFIG.C_NUM_OF_PROBES 5 \
+    CONFIG.C_PROBE0_WIDTH 1 \
+    CONFIG.C_PROBE1_WIDTH 41 \
+    CONFIG.C_PROBE2_WIDTH 1 \
+    CONFIG.C_PROBE3_WIDTH 16 \
+    CONFIG.C_PROBE4_WIDTH 1 \
+] [get_ips ila_0]
+
+# Create ila_1
+
+create_ip -name ila -vendor xilinx.com -library ip -module_name ila_1
+
+set_property -dict [list \
+    CONFIG.C_NUM_OF_PROBES 8 \
+    CONFIG.C_PROBE0_WIDTH 1 \
+    CONFIG.C_PROBE1_WIDTH 41 \
+    CONFIG.C_PROBE2_WIDTH 1 \
+    CONFIG.C_PROBE3_WIDTH 23 \
+    CONFIG.C_PROBE4_WIDTH 16 \
+    CONFIG.C_PROBE5_WIDTH 23 \
+    CONFIG.C_PROBE6_WIDTH 14 \
+    CONFIG.C_PROBE7_WIDTH 1 \
+] [get_ips ila_1]
+
+# Create ila_2
+
+create_ip -name ila -vendor xilinx.com -library ip -module_name ila_2
+
+set_property -dict [list \
+    CONFIG.C_NUM_OF_PROBES 4 \
+    CONFIG.C_PROBE0_WIDTH 1 \
+    CONFIG.C_PROBE1_WIDTH 41 \
+    CONFIG.C_PROBE2_WIDTH 23 \
+    CONFIG.C_PROBE3_WIDTH 1 \
+] [get_ips ila_2]
+
+# Generate IP targets
+
+generate_target all [get_ips]
+
+# Run synthesis, PnR, and generate the bitstream
+
+launch_runs impl_1 -to_step write_bitstream -jobs 2
+wait_on_run impl_1
